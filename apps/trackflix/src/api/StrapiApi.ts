155cb92927a0ee83836ccbc06f185535e9476dfe
Nathan de Balthasar <nathan.de-balthasar-de-gacheo@epitech.eu>
2023-09-25 09:49:23 +0200

    feat(strapi-integration): add StrapiApi class

--- apps/trackflix/src/api/StrapiApi.ts
+++ apps/trackflix/src/api/StrapiApi.ts
@@ -2,6 +2,15 @@ import { VideoOnDemand } from '../models'
 import { IApi } from './IApi'
 
 export class StrapiApi implements IApi {
+    private readonly endpoint: string
+
+    private readonly token: string
+
+    constructor(endpoint: string, token: string) {
+        this.endpoint = endpoint
+        this.token = token
+    }
+
     async fetchHighlightedVideos(): Promise<VideoOnDemand[]> {
         return []
     }
