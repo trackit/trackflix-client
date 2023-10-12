232e32e43dfda899af237fe4de35eb98722b0b09
Nathan de Balthasar <nathan.de-balthasar-de-gacheo@epitech.eu>
2023-10-12 15:30:59 +0200

    style(strapi/search-page): fix linting errors

--- apps/trackflix/src/shared/api/helper.ts
+++ apps/trackflix/src/shared/api/helper.ts
@@ -3,11 +3,11 @@ import { GRAPHQL_AUTH_MODE } from '@aws-amplify/api-graphql'
 
 async function getAuthMode() {
     return null
-//     await refreshUserCredentials()
+    //     await refreshUserCredentials()
 
-//     return Auth.Credentials.getCredSource() !== 'userPool'
-//         ? GRAPHQL_AUTH_MODE.AWS_IAM
-//         : GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS
+    //     return Auth.Credentials.getCredSource() !== 'userPool'
+    //         ? GRAPHQL_AUTH_MODE.AWS_IAM
+    //         : GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS
 }
 
 async function refreshUserCredentials() {
