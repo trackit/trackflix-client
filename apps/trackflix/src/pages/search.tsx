d5b19e88cd69352a44dbb6fe4ff856de4e0fc070
Nathan de Balthasar <nathan.de-balthasar-de-gacheo@epitech.eu>
2023-10-12 16:55:57 +0200

    style(strapi/section-page): fix linting errors

--- apps/trackflix/src/pages/search.tsx
+++ apps/trackflix/src/pages/search.tsx
@@ -83,13 +83,14 @@ type VideoItemProps = {
 }
 
 const VideoItem = ({ vod }: VideoItemProps) => {
-    const [thumbnail, setThumbnail] = useState<
-        | {
-              obj: Thumbnail
-              url: string
-          }
-        | undefined
-    >(undefined)
+    const [thumbnail, setThumbnail] =
+        useState<
+            | {
+                  obj: Thumbnail
+                  url: string
+              }
+            | undefined
+        >(undefined)
     const { width } = useWindowDimensions()
     const videoCardProperties = useMemo(() => {
         if (defaultVideoCardProperties.width > width - 100) {
